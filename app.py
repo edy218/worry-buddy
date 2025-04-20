9cb8969c639df2226322e2d66f251fc79ef0873e
edy218 <119718401+edy218@users.noreply.github.com>
2025-04-20 01:19:07 +0000

    COMPLETE RESET: Fresh start

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "HELLO WORLD"
+    return "Fresh start successful!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
